d05f68b4f2e6774d15135d2ab1ec54cec587b852
AntoineRoll <antoine-rollet@live.fr>
2024-05-20 14:28:29 +0200

    Start AiTraining Job operator

---
+++ ovhcloud/operators/ai_training.py
@@ -0,0 +1,31 @@
+from typing import Optional
+from airflow.models import BaseOperator
+from ovhcloud.hooks.api import OvhcloudApiHook
+
+
+class AiTrainingJobOperator(BaseOperator):
+    
+    def __init__(
+        self, 
+        ovhcloud_conn_id: str = 'ovhcloud_default',
+        project_id: Optional[str] = None,
+        **kwargs
+        ):
+        super().__init__(**kwargs)
+        
+        self.project_id = project_id
+        self.conn_id = ovhcloud_conn_id
+        
+        
+        
+    def execute(self, context):
+        
+        ovh_api_hook = OvhcloudApiHook(
+            ovhcloud_conn_id=self.conn_id
+        )
+        
+        if self.project_id is not None:
+            ovh_api_hook.project_id = None
+        
+        
+        
